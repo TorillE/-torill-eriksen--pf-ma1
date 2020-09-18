e425bf9686c42baf1109a7bb5854e3fd159a20ef
TorillE <71519573+TorillE@users.noreply.github.com>
2020-09-18 19:52:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// Declare a variable called firstVar but don't initialise it with a value.
+
+var firstVar;
+
+// Declare a variable called name and assign it your first name.
+
+var name = "Torill";
+
+// Declare and initialise a variable with a number value.
+
+var myNumer = 2;
+
+// Create a variable called division and initialise it with a value of 20 divided by 5.
+
+var division = (20/5);
+
+// Write code that checks the type of the value "frog".
+
+var slimyAnimal = "frog";
+
+typeof(slimyAnimal);
+
+// Declare and initialise a variable called orderHasShipped with a boolean value.
+
+var orderHasShipped = true;
+
+
+// Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+
+if (orderHasShipped = true) { 
+    console.log ("true");
+}
+else {
+    console.log("false");
+}
+
+// Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+
+for(var i = 0; i <= 9; i++) {
+    console.log(i);
+}
